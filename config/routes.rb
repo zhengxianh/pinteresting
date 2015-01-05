4ff282da6c50b99302593eee8433624530feb724
zheng <619913512hzx@gmail.com>
2015-01-04 21:19:52 -0500

    Creating home and bout pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinteresting::Application.routes.draw do
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
